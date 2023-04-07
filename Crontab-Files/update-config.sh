4bb519aed51f72a8a5cb7a422807a254a0a46aa0
DustinWin <45238096+DustinWin@users.noreply.github.com>
2023-04-07 08:28:52 +0800

    Update update-config.sh

--- Crontab-Files/update-config.sh
+++ Crontab-Files/update-config.sh
@@ -2,4 +2,5 @@ curl -o $clashdir/GeoSite.dat -L https://fastly.jsdelivr.net/gh/DustinWin/clash-
 curl -o $clashdir/GeoIP.dat -L https://fastly.jsdelivr.net/gh/DustinWin/clash-geoip@release/geoip.dat
 curl -o $clashdir/Country.mmdb -L https://fastly.jsdelivr.net/gh/DustinWin/clash-geoip@release/Country.mmdb
 curl -o $clashdir/user.yaml -L https://fastly.jsdelivr.net/gh/DustinWin/Clash-Files@main/MyConfig/user.yaml
-$clashdir/start.sh getyaml && $clashdir/start.sh restart
+curl -o $clashdir/clash -L https://fastly.jsdelivr.net/gh/DustinWin/Clash-Files@main/Clash.Meta-Core/clash.meta-linux-arm64
+chmod +x $clashdir/clash && $clashdir/start.sh getyaml && $clashdir/start.sh restart
